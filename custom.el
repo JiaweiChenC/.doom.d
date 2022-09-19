47a4b9a409fd3f4b1d019dba8f748bba8fa57335
JiaweiChenC <jc5667@columbia.edu>
2022-09-19 11:01:32 -0400

    Use python instead of ipython in babel

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-babel-python-command "ipython --simple-prompt" t)
+ ;; '(org-babel-python-command "ipython --simple-prompt" t)
  '(package-selected-packages
    '(ox-reveal latex-preview-pane rg cdlatex nov srcery-theme jupyter))
  '(python-shell-interpreter "ipython3")
