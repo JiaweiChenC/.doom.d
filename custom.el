ff927772e244d6e6367063b08bb0da4cdc728ee1
JiaweiChenC <jc5667@columbia.edu>
2025-12-09 09:21:03 -0500

    update local safe dir

--- custom.el
+++ custom.el
@@ -31,11 +31,12 @@
      "69a25b1d88aad37bb3a7c311a4a648772fa794a8ab4c1fa69b9455112e536449"
      "a8347c492511ecc002df26d2b268f1342d84af50c43901e0d88dd678134729d8"
      "cfc1e49eec0d57e2f70ef3a6db3b543bbb2edcf16a9f164231afcc4d1263f9fb" default))
- '(org-agenda-files '("/Users/jiawei/org/journal/20251201") t)
+ '(org-agenda-files '("/Users/jiawei/org/journal/20251201"))
  '(package-selected-packages
    '(cond-let llama magit-section tomlparse uuidgen with-editor))
  '(safe-local-variable-directories
-   '("/ssh:100.105.242.51:/home/jiawei/projects/GVHMR/"
+   '("/scpx:100.105.242.51:/home/jiawei/projects/SKEL/"
+     "/ssh:100.105.242.51:/home/jiawei/projects/GVHMR/"
      "/rsync:100.105.242.51:/home/jiawei/projects/GVHMR/"
      "/scpx:100.105.242.51:/home/jiawei/projects/GVHMR/"
      "/ssh:100.105.242.51:/home/jiawei/projects/HSMR/"
