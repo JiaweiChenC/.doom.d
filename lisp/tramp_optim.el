83fe60314769269d47c59d9f0d71037ef3538ded
JiaweiChenC <jc5667@columbia.edu>
2025-12-11 10:31:23 -0500

    update tramp optim

--- lisp/tramp_optim.el
+++ lisp/tramp_optim.el
@@ -1,61 +1,116 @@
-;;; lisp/tramp_optim.el -*- lexical-binding: t; -*-
+;;; tramp_optim.el --- TRAMP / project root optimizations -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; - Make TRAMP quieter/noisier via `tramp-verbose'.
+;; - Avoid VC poking at TRAMP paths.
+;; - Disable diff-hl on remote buffers.
+;; - Provide a jc/project-root abstraction that:
+;;   * Uses a host-local project root from dir-locals
+;;   * Prepends the appropriate TRAMP prefix on remote buffers
+;; - Integrate that root with Doom projects, Projectile, dumb-jump, and mini-echo.
+
+;;; Code:
+
+;;---------------------------------------------------------------------------
+;; Basic TRAMP tweaks
+;;---------------------------------------------------------------------------
+
 (setq! tramp-verbose 2)
 
-;; tramp trick 
+;; Don't let VC wander into TRAMP paths.
 (setq vc-ignore-dir-regexp
       (format "\\(%s\\)\\|\\(%s\\)"
               vc-ignore-dir-regexp
               tramp-file-name-regexp))
 
+;;---------------------------------------------------------------------------
+;; diff-hl: disable on remote
+;;---------------------------------------------------------------------------
+
 (after! diff-hl
   (defun jc/diff-hl-disable-on-remote ()
+    "Disable diff-hl in remote TRAMP buffers."
     (when (file-remote-p default-directory)
       (diff-hl-mode -1)
       (diff-hl-flydiff-mode -1)))
-
-;;   ;; In case something else turns it on, we turn it back off for remote
+  ;; In case something else turns it on, we turn it back off for remote.
   (add-hook 'find-file-hook #'jc/diff-hl-disable-on-remote))
-;;; Configurable remote project roots via dir-locals
-;;; ---------------------------------------------------------------------------
 
-(defvar-local jc/project-root nil
-  "Per-buffer override for Doom / project root (especially for TRAMP).")
+;;---------------------------------------------------------------------------
+;; Project root abstraction
+;;---------------------------------------------------------------------------
 
-(defvar jc/remote-project-roots
-  '("/scpx:100.105.242.51:/home/jiawei/projects/GVHMR/")
-  "List of remote project root directories.
+(defvar-local jc/project-root-local nil
+  "Host-local project root for this buffer, e.g. \"/home/jiawei/projects/SKEL/\".
+No TRAMP prefix; that is reconstructed from `default-directory' when needed.")
 
-For each entry, we set up a dir-locals class that binds
-`jc/project-root` and `projectile-project-root` to that path.")
+(defun jc/project-root ()
+  "Return full project root path for the current buffer.
 
-(defun jc/register-remote-project-roots ()
-  "Create dir-locals classes for `jc/remote-project-roots`."
-  (dolist (root jc/remote-project-roots)
-    ;; One class per root, so each can have its own value.
-    (let* ((class (intern (format "jc-remote-%s" (md5 root))))
-           (vars  `((nil . ((jc/project-root . ,root)
-                            ;; (projectile-project-root . ,root)
-                            )))))
-      (dir-locals-set-class-variables class vars)
-      (dir-locals-set-directory-class root class))))
+If `jc/project-root-local' is set, combine it with the TRAMP prefix when
+the buffer is remote; otherwise return it as-is.  Return nil if unset."
+  (when (and (boundp 'jc/project-root-local)
+             (stringp jc/project-root-local))
+    (let ((prefix (file-remote-p default-directory)))
+      (if prefix
+          (concat prefix jc/project-root-local)
+        jc/project-root-local))))
 
-;; Run once at startup; rerun if you change `jc/remote-project-roots`.
-(jc/register-remote-project-roots)
+;;---------------------------------------------------------------------------
+;; Multiple remote servers: dir-locals registration
+;;---------------------------------------------------------------------------
 
-;;; ---------------------------------------------------------------------------
-;;; Doom project integration (unchanged logic)
-;;; ---------------------------------------------------------------------------
+(defvar jc/remote-project-specs
+  '(("/scpx:100.105.242.51:"
+     ;; "/home/jiawei/projects/GVHMR/"
+     ;; "/home/jiawei/projects/SKEL/"
+     ;; "/home/jiawei/projects/HSMR/"
+     ))
+  "List of remote project specs.
+
+Each element is a list: (TRAMP-PREFIX ROOT1 ROOT2 ...), where ROOT*
+are host-local directories on that server (no TRAMP prefix).")
+
+;; (defun jc/register-remote-project-roots ()
+;;   "Create dir-locals classes for all `jc/remote-project-specs'.
+
+;; Each root is host-local; we build the full TRAMP directory using the
+;; given prefix, but `jc/project-root-local' stays host-local."
+;;   (dolist (spec jc/remote-project-specs)
+;;     (let ((prefix (car spec))
+;;           (roots  (cdr spec)))
+;;       (dolist (root roots)
+;;         (let* ((remote-root (concat prefix root))
+;;                (class (intern (format "jc-remote-%s" (md5 remote-root))))
+;;                (vars  `((nil . ((jc/project-root-local . ,root))))))
+;;           (dir-locals-set-class-variables class vars)
+;;           (dir-locals-set-directory-class remote-root class))))))
+
+;; ;; Run once at startup; rerun if you change `jc/remote-project-specs`.
+;; (jc/register-remote-project-roots)
+
+;; Example to add another server later:
+;;
+;; (setq jc/remote-project-specs
+;;       '(("/scpx:100.105.242.51:"
+;;          "/home/jiawei/projects/GVHMR/"
+;;          "/home/jiawei/projects/SKEL/"
+;;          "/home/jiawei/projects/HSMR/")
+;;         ("/scpx:other.host:"
+;;          "/home/jiawei/projects/OTHER/")))
+;; (jc/register-remote-project-roots)
+
+;;---------------------------------------------------------------------------
+;; Doom project integration
+;;---------------------------------------------------------------------------
 
 (defun jc/doom-project-root-from-dir-locals (orig-fun &optional dir &rest _)
-  "If `jc/project-root` matches DIR (and DIR is remote), use it;
-else call ORIG-FUN."
+  "Use `jc/project-root' for remote DIR when applicable; fall back to ORIG-FUN."
   (let* ((dir  (or dir default-directory))
-         (root (and (bound-and-true-p jc/project-root)
-                    (stringp jc/project-root)
-                    jc/project-root)))
+         (root (jc/project-root)))
     (if (and root
              (file-remote-p dir)
-             ;; Only override when DIR is really under our remote root
+             ;; Only override when DIR is really under our remote root.
              (string-prefix-p (file-name-as-directory root)
                               (file-name-as-directory dir)))
         root
@@ -63,64 +118,82 @@ else call ORIG-FUN."
 
 (advice-add #'doom-project-root :around #'jc/doom-project-root-from-dir-locals)
 
-;;; ---------------------------------------------------------------------------
-;;; Projectile integration (unchanged logic)
-;;; ---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
+;; Projectile integration
+;;---------------------------------------------------------------------------
 
-;; (with-eval-after-load 'projectile
-(defun jc/projectile-project-root-remote-orig (orig-fn &optional dir)
-    "Use `jc/project-root` for remote buffers, fall back to ORIG-FN otherwise."
-    (let* ((dir (or dir default-directory)))
-      (if (file-remote-p dir)
-          ;; Remote: try our explicit root, fall back to DIR itself.
-          (or jc/project-root
-              ;; Last-resort: don't scan upward, just treat current dir as root.
-              (file-name-as-directory dir))
-        ;; Local: normal Projectile behaviour.
-        (funcall orig-fn dir))))
+(defun jc/projectile-project-root-remote (orig-fn &optional dir)
+  "Use `jc/project-root' for remote buffers, fall back to ORIG-FN otherwise."
+  (let* ((dir  (or dir default-directory))
+         (root (jc/project-root)))
+    (if (file-remote-p dir)
+        ;; Remote: try our explicit root, fall back to DIR itself.
+        (or root
+            ;; Last-resort: treat current dir as root, no upward scan.
+            (file-name-as-directory dir))
+      ;; Local: normal Projectile behaviour.
+      (funcall orig-fn dir))))
 
-  (advice-add 'projectile-project-root :around
-              #'jc/projectile-project-root-remote-orig)
-;; )
+(advice-add #'projectile-project-root :around #'jc/projectile-project-root-remote)
 
-;;; ---------------------------------------------------------------------------
-;;; dumb-jump integration (unchanged logic)
-;;; ---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
+;; dumb-jump integration
+;;---------------------------------------------------------------------------
 
 (after! dumb-jump
   (defun jc/dumb-jump-project-root-remote (orig-fn &rest args)
-    "Avoid `locate-dominating-file` on TRAMP by not calling ORIG-FN there.
-On remote files, use `jc/project-root` if set, otherwise `default-directory`."
+    "Avoid heavy project root detection on TRAMP.
+
+On remote files, use `jc/project-root' if set, otherwise `default-directory'.
+On local files, defer to ORIG-FN."
     (if (file-remote-p default-directory)
-        ;; REMOTE: do NOT call orig-fn (which uses locate-dominating-file).
-        (or (and (boundp 'jc/project-root) jc/project-root)
+        ;; REMOTE: do NOT call ORIG-FN (which uses locate-dominating-file).
+        (or (jc/project-root)
             (file-name-as-directory
              (directory-file-name default-directory)))
       ;; LOCAL: normal behavior.
       (apply orig-fn args)))
-
-  (advice-add 'dumb-jump-get-project-root
+  (advice-add #'dumb-jump-get-project-root
               :around #'jc/dumb-jump-project-root-remote))
 
+;;---------------------------------------------------------------------------
+;; mini-echo integration
+;;---------------------------------------------------------------------------
+
 (defun jc/mini-echo-project-root ()
-  "Project root for mini-echo, consistent with jc/project-root / Doom."
+  "Project root for mini-echo, consistent with `jc/project-root' / Doom."
   (or
-   ;; Explicit override for remote etc.
-   (and (boundp 'jc/project-root) jc/project-root)
-   ;; Doom's project root (already advised to use jc/project-root on remote)
+   ;; Explicit override for remote/local via jc/project-root-local.
+   (jc/project-root)
+   ;; Doom's project root (already advised to use jc/project-root on remote).
    (when (fboundp 'doom-project-root)
      (doom-project-root))
-   ;; Last fallback
+   ;; Last fallback: just use the current directory.
    (file-name-as-directory
     (directory-file-name default-directory))))
 
 (setq mini-echo-project-detection #'jc/mini-echo-project-root)
 
+;; (provide 'tramp_optim)
+;;; tramp_optim.el ends here
 
-(setq jc/remote-project-roots
-      '("/scpx:100.105.242.51:/home/jiawei/projects/GVHMR/"
-        "/scpx:100.105.242.51:/home/jiawei/projects/SKEL/"
-        "/scpx:100.105.242.51:/home/jiawei/projects/HSMR/"))
 
-(jc/register-remote-project-roots)  ;; rerun after changing the list
+;; add scpx login in to vterm tramp shells 
+(after! vterm
+  (add-to-list 'vterm-tramp-shells '("scpx" login-shell) t))
 
+;; enable cache udpating on remote, we will see whether is is a performance issue
+(after! projectile
+  (defun jc/projectile-find-file-hook-function ()
+    "Like `projectile-find-file-hook-function' but also for remote files."
+    (projectile-maybe-limit-project-file-buffers)
+    (when projectile-dynamic-mode-line
+      (projectile-update-mode-line))
+    (when projectile-auto-update-cache
+      (projectile-cache-files-find-file-hook))
+    (projectile-track-known-projects-find-file-hook)
+    (projectile-visit-project-tags-table))
+
+  ;; Replace Projectile’s version with ours
+  (remove-hook 'find-file-hook #'projectile-find-file-hook-function)
+  (add-hook 'find-file-hook #'jc/projectile-find-file-hook-function))
